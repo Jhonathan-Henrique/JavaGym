f1a67e26348953e6dfc7b4dc4ae9890cb6d9c605
Jhonathan <Jhonathan.Engineer@gmail.com>
2024-11-20 12:57:35 -0300

    Chore: Add challenger two notes decimals

---
+++ Desafio duas notas decimais/desafioDuasNotasDecimais/src/desafio.java
@@ -0,0 +1,11 @@
+//Crie um programa que realize a média de duas notas decimais e exiba o resultado.
+public class desafio {
+    public static void main (String [] args) {
+        double nota1 = 5.2;
+        double nota2 = 10.5;
+
+        double media = (nota1 + nota2) / 2;
+        System.out.println(media);
+    }
+}
+
