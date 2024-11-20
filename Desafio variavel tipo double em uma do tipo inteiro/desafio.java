ec16dd37a8d56a558c1f15d7bb7f6856f0c8f762
Jhonathan <Jhonathan.Engineer@gmail.com>
2024-11-20 13:06:37 -0300

    teste

---
+++ Desafio variavel tipo double em uma do tipo inteiro/desafio.java
@@ -0,0 +1,10 @@
+// Declare uma variável do tipo double e uma variável do tipo int. Faça o casting da
+// variável double para int e imprima o resultado.
+public class desafio {
+    public static void main(String[] args){
+        double numeroDouble = 1.0;
+        int receberDouble = (int) numeroDouble;
+
+        System.out.println(receberDouble);
+    }
+}
